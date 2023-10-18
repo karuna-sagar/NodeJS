e9cacd1e5440f4996b87cead489ffc98fa6d2b92
Karuna sagar <karunasagar129130@gmail.com>
2023-10-18 22:36:24 +0530

    route created

--- Online Shop/app.js
+++ Online Shop/app.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const adminRotes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"<button type="submit">Add Product</button></form>'
-  );
-});
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+app.use(adminRotes);
+app.use(shopRoutes);
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello From express js</h1>");
-});
+// app.use();
 app.listen(3000);
