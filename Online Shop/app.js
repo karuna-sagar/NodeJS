1248615fea77468fd18e3a0ced63a97073259c03
Karuna sagar <karunasagar129130@gmail.com>
2023-10-18 23:21:13 +0530

    admin page created

--- Online Shop/app.js
+++ Online Shop/app.js
@@ -4,8 +4,11 @@ const app = express();
 const adminRotes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(adminRotes);
+app.use("/admin", adminRotes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
+});
 // app.use();
 app.listen(3000);
