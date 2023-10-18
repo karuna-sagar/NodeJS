a0280ecf799968d08c8af836fed9b6196bb66392
Karuna sagar <karunasagar129130@gmail.com>
2023-10-19 00:34:03 +0530

    directory created

--- Online Shop/routes/admin.js
+++ Online Shop/routes/admin.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const rootDir = require("../utils/path");
 const router = express.Router();
 const path = require("path");
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 router.post("/product", (req, res, next) => {
   console.log(req.body);
--- Online Shop/routes/shop.js
+++ Online Shop/routes/shop.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const rootDir = require("../utils/path");
 router.get("/", (req, res, next) => {
   res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
