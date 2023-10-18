5b185028b6dc7bee0949bab5af3fd6ca8d0967df
Karuna sagar <karunasagar129130@gmail.com>
2023-10-19 00:25:57 +0530

    static page

--- Online Shop/app.js
+++ Online Shop/app.js
@@ -3,12 +3,13 @@ const bodyParser = require("body-parser");
 const app = express();
 const adminRotes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const path = require("path");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/admin", adminRotes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page Not Found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 // app.use();
 app.listen(3000);
--- Online Shop/routes/admin.js
+++ Online Shop/routes/admin.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
-
+const path = require("path");
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/admin/product" method="POST"><input type="text" name="title"<button type="submit">Add Product</button></form>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 router.post("/product", (req, res, next) => {
   console.log(req.body);
