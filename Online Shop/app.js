34167f8f1c58fd859a8baaf5426f0a7ca865c5b6
Karuna sagar <karunasagar129130@gmail.com>
2023-10-21 22:50:45 +0530

    pug file

--- Online Shop/app.js
+++ Online Shop/app.js
@@ -1,19 +1,24 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const app = express();
-const adminRotes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const path = require("path");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "pug");
 app.set("views", "views");
-app.use("/admin", adminRotes);
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
-// app.use();
+
 app.listen(3000);
--- Online Shop/routes/shop.js
+++ Online Shop/routes/shop.js
@@ -1,9 +1,15 @@
-const express = require("express");
-const router = express.Router();
 const path = require("path");
+
+const express = require("express");
+
 const rootDir = require("../utils/path");
+const adminData = require("./admin");
+
+const router = express.Router();
+
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
