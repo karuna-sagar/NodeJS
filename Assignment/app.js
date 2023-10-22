3bca9a1917389b07784bcecaaefccd218718e371
Karuna sagar <karunasagar129130@gmail.com>
2023-10-22 18:38:52 +0530

    first commit

--- Assignment/app.js
+++ Assignment/app.js
@@ -1,18 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
+const users = [];
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "pug");
 app.set("views", "views");
 
 app.get("/", (req, res, next) => {
-  res.render();
+  res.render("index", { titleDoc: "Add User" });
 });
 
 app.get("/users", (req, res, next) => {
-  res.render();
+  res.render("user", { titleDoc: "Users", users: users });
 });
 
 app.post("/add-user", (req, res, next) => {
-  res.render();
+  users.push({ name: req.body.username });
+  res.redirect("/users");
 });
 app.listen(3000);
