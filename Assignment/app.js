ee87a10714592484dce29a7b3a1bf2f61e335d35
Karuna sagar <karunasagar129130@gmail.com>
2023-10-22 13:16:59 +0530

    main-layout

--- Assignment/app.js
+++ Assignment/app.js
@@ -3,4 +3,16 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", "views");
+
+app.get("/", (req, res, next) => {
+  res.render();
+});
+
+app.get("/users", (req, res, next) => {
+  res.render();
+});
+
+app.post("/add-user", (req, res, next) => {
+  res.render();
+});
 app.listen(3000);
