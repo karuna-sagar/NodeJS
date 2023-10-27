6f19c1a96a3a6c8f0fd9c475560d9ff9f38505c1
Karuna sagar <karunasagar129130@gmail.com>
2023-10-28 01:02:37 +0530

    cart are created

--- Online Shop/models/product.js
+++ Online Shop/models/product.js
@@ -49,8 +49,12 @@ module.exports = class Product {
 
   static deleteById(id) {
     getProductsFromFile((products) => {
-      const productIndex = products.findIndex(prod => prod)
-    })
+      const updatedProducts = products.filter((prod) => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+        }
+      });
+    });
   }
 
   static fetchAll(cb) {
