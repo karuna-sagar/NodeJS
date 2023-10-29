e64008a9a388eedff5bb04e66ef686210ab4d60d
Karuna sagar <karunasagar129130@gmail.com>
2023-10-29 23:41:51 +0530

    database connect with app

--- Online Shop/app.js
+++ Online Shop/app.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const db = require("./util/database");
+const sequelize = require("./util/database");
 const errorController = require("./controllers/error");
 const app = express();
 
@@ -16,7 +16,14 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
-
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
