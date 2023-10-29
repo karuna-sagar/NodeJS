e8d3d16d7306ef52a04189770da095518269447b
Karuna sagar <karunasagar129130@gmail.com>
2023-10-29 11:32:24 +0530

    database connect

--- Online Shop/controllers/shop.js
+++ Online Shop/controllers/shop.js
@@ -89,6 +89,6 @@ exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId, (product) => {
     Cart.deleteProduct(prodId, product.price);
-    res.redirect("/");
+    res.redirect("/cart");
   });
 };
