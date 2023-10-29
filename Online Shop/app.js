6d407f7d6d7a9c5bf47dc0511c25960c7da1576b
Karuna sagar <karunasagar129130@gmail.com>
2023-10-29 12:53:56 +0530

    database completed

--- Online Shop/app.js
+++ Online Shop/app.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const db = require("./util/database");
 const errorController = require("./controllers/error");
 const app = express();
 
@@ -11,6 +11,13 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
+db.execute("select * from products")
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
