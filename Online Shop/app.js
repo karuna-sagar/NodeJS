4510e376f9a990b39bbf624692764afb89854a7c
Karuna sagar <karunasagar129130@gmail.com>
2023-11-12 15:23:04 +0530

    user model created

--- Online Shop/app.js
+++ Online Shop/app.js
@@ -13,16 +13,39 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const User = require('./models/user');
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+    User.findById('65509deb367e503d1e115035')
+        .then(user => {
+            req.user = user;
+            next();
+        }).catch(err => console.log(err));
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(result => {
-        console.log('conected to Database')
+        console.log('conected to Database');
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'karuna',
+                    email: 'karunasagar123@gmail.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        })
+
+
         app.listen(3000)
     }).catch(err => console.log(err));
--- Online Shop/models/user.js
+++ Online Shop/models/user.js
@@ -1,16 +1,20 @@
-const Sequelize = require('sequelize');
-
-const sequelize = require('../util/database');
-
-const User = sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
     },
-    name: Sequelize.STRING,
-    email: Sequelize.STRING
-});
+    email: {
+        type: String,
+        required: true
+    },
+    cart: {
+        items: [{
+            productId: { type: Schema.Types.ObjectId, required: true },
+            quantity: { type: Number, required: true }
+        }]
+    }
+})
 
-module.exports = User;
+module.exports = mongoose.model('User', userSchema)
