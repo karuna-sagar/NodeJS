9bea62105faaac6589dab950ac4531f9d5f9df23
Karuna sagar <karunasagar129130@gmail.com>
2023-11-14 20:01:45 +0530

    signup update

--- Online Shop/controllers/auth.js
+++ Online Shop/controllers/auth.js
@@ -9,6 +9,14 @@ exports.getLogin = (req, res, next) => {
     });
 };
 
+exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        isAuthenticated: false
+    });
+};
+
 exports.postLogin = (req, res, next) => {
     User.findById('65509deb367e503d1e115035')
         .then(user => {
@@ -20,7 +28,7 @@ exports.postLogin = (req, res, next) => {
         })
         .catch(err => console.log(err));
 };
-
+exports.postSignup = (req, res, next) => { };
 exports.postLogout = (req, res, next) => {
     req.session.destroy(err => {
         // console.log(err);
