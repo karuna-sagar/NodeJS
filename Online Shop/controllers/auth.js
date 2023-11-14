cdd3ded3592d2641928aef8346393299f98d2962
Karuna sagar <karunasagar129130@gmail.com>
2023-11-14 12:45:34 +0530

    authentication through session

--- Online Shop/controllers/auth.js
+++ Online Shop/controllers/auth.js
@@ -1,9 +1,6 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-    //   const isLoggedIn = req
-    //     .get('Cookie')
-    //     .split(';')[1]
-    //     .trim()
-    //     .split('=')[1] === 'true';
     // console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/login',
@@ -13,6 +10,18 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    req.session.isLoggedIn = true;
-    res.redirect('/');
+    User.findById('65509deb367e503d1e115035')
+        .then(user => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            res.redirect('/');
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    });
 };
