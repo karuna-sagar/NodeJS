9d8ba9149148cb57704480c0171640f05cd103a3
Karuna sagar <karunasagar129130@gmail.com>
2023-11-15 16:12:08 +0530

    csfr

--- Online Shop/app.js
+++ Online Shop/app.js
@@ -52,6 +52,11 @@ app.use((req, res, next) => {
             next();
         }).catch(err => console.log(err));
 })
+
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn,
+        res.locals.csrfToken = req.csrfToken();
+})
 app.use('/admin', adminRoutes);
 app.use(shopRoutes)
 app.use(authRoutes);
