9aa59e6348a66ca46fc2dbb2bdda0bdc48f6823b
Karuna sagar <karunasagar129130@gmail.com>
2023-11-15 11:31:36 +0530

    final

--- Online Shop/controllers/auth.js
+++ Online Shop/controllers/auth.js
@@ -18,13 +18,30 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    User.findById('65509deb367e503d1e115035')
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({ email: email })
         .then(user => {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            req.session.save(err => {
-                res.redirect('/');
-            });
+            if (!user) {
+                return res.redirect('/login');
+            }
+            bcrypt
+                .compare(password, user.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        req.session.isLoggedIn = true;
+                        req.session.user = user;
+                        return req.session.save(err => {
+                            console.log(err);
+                            res.redirect('/');
+                        });
+                    }
+                    res.redirect('/login');
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.redirect('/login');
+                });
         })
         .catch(err => console.log(err));
 };
