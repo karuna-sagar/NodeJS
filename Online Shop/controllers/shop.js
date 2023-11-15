be4d7253ee5d8e7f81d39e7673f924c5811e8c3e
Karuna sagar <karunasagar129130@gmail.com>
2023-11-15 21:12:30 +0530

    cart button working

--- Online Shop/controllers/shop.js
+++ Online Shop/controllers/shop.js
@@ -93,7 +93,7 @@ exports.postOrder = (req, res, next) => {
       });
       const order = new Order({
         user: {
-          name: req.user.name,
+          email: req.user.email,
           userId: req.user
         },
         products: products
@@ -101,7 +101,7 @@ exports.postOrder = (req, res, next) => {
       return order.save();
     })
     .then(result => {
-      return req.user.clearCart();
+      return req.user.ClearCart();
     })
     .then(() => {
       res.redirect('/orders');
