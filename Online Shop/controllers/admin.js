3489ee62926ef8c1aaf5cc74788f4911339ded30
Karuna sagar <karunasagar129130@gmail.com>
2023-11-20 01:18:44 +0530

    The diff adds error handling to the following routes:
    - `exports.postEditProduct`
    - `exports.getProducts`
    - `exports.postDeleteProduct`
    
    The error handling catches errors and passes them to the next middleware function.

--- Online Shop/controllers/admin.js
+++ Online Shop/controllers/admin.js
@@ -125,7 +125,11 @@ exports.postEditProduct = (req, res, next) => {
         res.redirect('/admin/products');
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.getProducts = (req, res, next) => {
@@ -141,7 +145,11 @@ exports.getProducts = (req, res, next) => {
         path: '/admin/products'
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postDeleteProduct = (req, res, next) => {
@@ -152,5 +160,9 @@ exports.postDeleteProduct = (req, res, next) => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
