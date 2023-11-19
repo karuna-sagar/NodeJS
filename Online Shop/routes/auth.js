430e367ecc277b63667746ac20fc0e2cb8fbeeb0
Karuna sagar <karunasagar129130@gmail.com>
2023-11-19 12:04:10 +0530

    confirm password

--- Online Shop/routes/auth.js
+++ Online Shop/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const { check, body } = require('express-validator')
 const authController = require('../controllers/auth');
 
 const router = express.Router();
@@ -10,7 +10,23 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post('/signup', [
+    check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email.'),
+    body(
+        'password',
+        'Please enter a password with only numbers and text and at least 4 characters.'
+    )
+        .isLength({ min: 4 })
+        .isAlphanumeric(),
+    body('confirmPassword').custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Passwords have to match!');
+        }
+        return true;
+    })
+], authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
