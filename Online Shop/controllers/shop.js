6747e7d4ffbca2ccf932d7b54758ccf888af8970
Karuna sagar <karunasagar129130@gmail.com>
2023-11-24 00:51:50 +0530

    Update code to get invoice and order id from params

--- Online Shop/controllers/shop.js
+++ Online Shop/controllers/shop.js
@@ -146,17 +146,28 @@ exports.getOrders = (req, res, next) => {
 
 exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
-  const invoiceName = 'invoice-' + orderId + '.pdf';
-  const invoicePath = path.join('data', 'invoices', invoiceName);
-  fs.readFile(invoicePath, (err, data) => {
-    if (err) {
-      return next(err);
+  Order.findById(orderId).then(order => {
+    if (!order) {
+      return next(new Error('no Order Found'));
     }
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader(
-      'Content-Disposition',
-      'inline; filename="' + invoiceName + '"'
-    );
-    res.send(data);
-  })
+    if (order.user.userId.toString() !== req.user._id.toString()) {
+      return next(new Error('No Authorized'));
+    }
+    const invoiceName = 'invoice-' + orderId + '.pdf';
+    const invoicePath = path.join('data', 'invoices', invoiceName);
+    fs.readFile(invoicePath, (err, data) => {
+      if (err) {
+        return next(err);
+      }
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', 'attachment; filename=quote.pdf');
+      // res.contentType("application/pdf")
+      // res.setHeader(
+      //   'Content-Disposition',
+      //   'inline; filename="' + invoiceName + '"'
+      // );
+      res.send(data);
+    })
+  }).catch(err => console.log(err))
+
 }
