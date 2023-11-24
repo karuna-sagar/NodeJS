d34f8785984f954051d83db660c3fc675a6c8d2b
Karuna sagar <karunasagar129130@gmail.com>
2023-11-24 19:50:34 +0530

    Update invoice generation logic and add a PDF generation library dependency

--- Online Shop/controllers/shop.js
+++ Online Shop/controllers/shop.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const Product = require('../models/product');
 const Order = require('../models/order');
-
+const PDFDocument = require('pdfkit')
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then(products => {
@@ -155,6 +155,13 @@ exports.getInvoice = (req, res, next) => {
     }
     const invoiceName = 'invoice-' + orderId + '.pdf';
     const invoicePath = path.join('data', 'invoices', invoiceName);
+    const pdfDoc = new PDFDocument();
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
+    pdfDoc.pipe(fs.createWriteStream(invoicePath));
+    pdfDoc.pipe(res);
+    pdfDoc.text('Hello World');
+    pdfDoc.end();
     // fs.readFile(invoicePath, (err, data) => {
     //   if (err) {
     //     return next(err);
@@ -164,10 +171,9 @@ exports.getInvoice = (req, res, next) => {
 
     //   res.send(data);
     // })
-    const file = fs.createReadStream(invoicePath);
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'attachment; filename=quote.pdf');
-    file.pipe(res)
+    // const file = fs.createReadStream(invoicePath);
+
+    // file.pipe(res)
 
   }).catch(err => console.log(err))
 
